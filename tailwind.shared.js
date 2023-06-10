e12d0d1acac5a65ca168e0caab474c3c05d8cfda
yuri-duque <yurithielmann83@gmail.com>
2023-06-10 00:44:31 -0300

    creating a tailwind shared config

---
+++ tailwind.shared.js
@@ -0,0 +1,24 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      condensed: ['"One Dot Condensed Bold"', '"Arial Narrow"', "Monaco"], //font-condensed
+    },
+    colors: {
+      white: "#FFF",
+      black: "#000",
+
+      primary: "#2196f3",
+      "primary-light": "#4dabf5",
+      "primary-dark": "#197fe3",
+
+      success: "#4caf50",
+      "success-light": "#6fbf73",
+      "success-dark": "#389c3c",
+
+      error: "#f44336",
+      "error-light": "#f6685e",
+      "error-dark": "#d14136",
+    },
+  },
+};
