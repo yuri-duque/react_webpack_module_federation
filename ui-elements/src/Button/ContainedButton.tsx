ccd97ef9107c6cb33c8dcee2aedaab679575490a
yuri-duque <yurithielmann83@gmail.com>
2023-06-10 00:46:08 -0300

    creating contained button component

---
+++ ui-elements/src/Button/ContainedButton.tsx
@@ -0,0 +1,81 @@
+import React, { ReactNode } from "react";
+
+export interface ButtonProps {
+  size?: "small" | "medium" | "large";
+  color?: "primary" | "success" | "error";
+  uppercase?: boolean;
+  bold?: boolean;
+  disabled?: boolean;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  children?: ReactNode;
+}
+
+export const Button = function ({
+  size = "medium",
+  color = "primary",
+  uppercase,
+  bold,
+  disabled,
+  onClick,
+  children,
+}: ButtonProps) {
+  const className = handleClassName(size, color, uppercase, bold, disabled);
+
+  return (
+    <button onClick={onClick} className={className}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
+
+function handleClassName(
+  size: string,
+  color: string,
+  uppercase?: boolean,
+  bold?: boolean,
+  disabled?: boolean
+) {
+  let className = `rounded cursor-pointer shadow-[2px_4px_1px_#cdcdcd] transition-colors duration-300`;
+
+  if (disabled) {
+    className += " opacity-50 cursor-not-allowed";
+  } else {
+    className += ` active:shadow-[2px_2px_1px_#9e9e9e] active:translate-y-0.5 `;
+  }
+
+  switch (color) {
+    case "primary":
+      className += " bg-primary hover:bg-primary-dark text-white";
+      break;
+
+    case "success":
+      className += " bg-success hover:bg-success-dark text-white";
+      break;
+
+    case "error":
+      className += " bg-error hover:bg-error-dark text-white";
+      break;
+  }
+
+  switch (size) {
+    case "small":
+      className += " text-sm py-1 px-2";
+      break;
+
+    case "medium":
+      className += " text-base py-2 px-4";
+      break;
+
+    case "large":
+      className += " text-lg py-4 px-8";
+      break;
+  }
+
+  if (uppercase) className += " uppercase";
+
+  if (bold) className += " font-bold";
+
+  return className;
+}
--- ui-elements/src/index.tsx
+++ ui-elements/src/index.tsx
@@ -1,12 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ContainedButton from "./Button/ContainedButton";
+
+import "./index.css";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="flex">
+      <div className="p-2">
+        <ContainedButton bold>enable</ContainedButton>
+      </div>
+      <div className="p-2">
+        <ContainedButton bold disabled>
+          disabled
+        </ContainedButton>
+      </div>
+    </div>
+    <div className="flex">
+      <div className="p-2">
+        <ContainedButton color="success" bold>
+          enable
+        </ContainedButton>
+      </div>
+      <div className="p-2">
+        <ContainedButton color="success" bold disabled>
+          disabled
+        </ContainedButton>
+      </div>
+    </div>
+    <div className="flex">
+      <div className="p-2">
+        <ContainedButton color="error" bold>
+          enable
+        </ContainedButton>
+      </div>
+      <div className="p-2">
+        <ContainedButton color="error" bold disabled>
+          disabled
+        </ContainedButton>
+      </div>
+    </div>
   </React.StrictMode>
 );
