1daa07dd47535ecc2fffb8ff76c61015a3a32256
yuri-duque <yurithielmann83@gmail.com>
2023-06-13 22:49:08 -0300

    int tailwind on component project

--- components/src/index.tsx
+++ components/src/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CartItem from "./CartItem";
+
+import "./index.css";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <CartItem quantity={1} add={() => {}} remove={() => {}} />
   </React.StrictMode>
 );
