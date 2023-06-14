4f7ef458b3cd0078f566d79d5b03ca2d20fd0080
yuri-duque <yurithielmann83@gmail.com>
2023-06-14 19:17:11 -0300

    creating a App page

---
+++ ui-elements/src/App.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import Card from "ui-elements/Card";
+import ContainedButton from "./Button/ContainedButton";
+
+const App = () => {
+  return (
+    <>
+      <div className="m-8">
+        <div className="flex">
+          <div className="p-2">
+            <ContainedButton>enable</ContainedButton>
+          </div>
+          <div className="p-2">
+            <ContainedButton disabled>disabled</ContainedButton>
+          </div>
+        </div>
+        <div className="flex">
+          <div className="p-2">
+            <ContainedButton color="success">enable</ContainedButton>
+          </div>
+          <div className="p-2">
+            <ContainedButton color="success" disabled>
+              disabled
+            </ContainedButton>
+          </div>
+        </div>
+        <div className="flex">
+          <div className="p-2">
+            <ContainedButton color="error">enable</ContainedButton>
+          </div>
+          <div className="p-2">
+            <ContainedButton color="error" disabled>
+              disabled
+            </ContainedButton>
+          </div>
+        </div>
+      </div>
+      <div className="m-8">
+        <div className="p-2">
+          <Card title="milk">
+            <div className="flex justify-between items-center">
+              <ContainedButton color="error">-</ContainedButton>
+              <p className="text-center font-bold px-4">0</p>
+              <ContainedButton color="success">+</ContainedButton>
+            </div>
+          </Card>
+        </div>
+        <div className="p-2">
+          <Card title="City Lights in New York" size="small">
+            Test
+          </Card>
+        </div>
+        <div className="p-2">
+          <Card title="City Lights in New York" size="medium">
+            Test
+          </Card>
+        </div>
+        <div className="p-2">
+          <Card title="City Lights in New York" size="large">
+            Test
+          </Card>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
--- ui-elements/src/index.tsx
+++ ui-elements/src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ContainedButton from "./Button/ContainedButton";
-import Card from "./Card/Card";
+import App from "./App";
 
 import "./index.css";
 
@@ -10,62 +9,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <div className="m-8">
-      <div className="flex">
-        <div className="p-2">
-          <ContainedButton>enable</ContainedButton>
-        </div>
-        <div className="p-2">
-          <ContainedButton disabled>disabled</ContainedButton>
-        </div>
-      </div>
-      <div className="flex">
-        <div className="p-2">
-          <ContainedButton color="success">enable</ContainedButton>
-        </div>
-        <div className="p-2">
-          <ContainedButton color="success" disabled>
-            disabled
-          </ContainedButton>
-        </div>
-      </div>
-      <div className="flex">
-        <div className="p-2">
-          <ContainedButton color="error">enable</ContainedButton>
-        </div>
-        <div className="p-2">
-          <ContainedButton color="error" disabled>
-            disabled
-          </ContainedButton>
-        </div>
-      </div>
-    </div>
-
-    <div className="m-8">
-      <div className="p-2">
-        <Card title="milk">
-          <div className="flex justify-between items-center">
-            <ContainedButton color="error">-</ContainedButton>
-            <p className="text-center font-bold px-4">0</p>
-            <ContainedButton color="success">+</ContainedButton>
-          </div>
-        </Card>
-      </div>
-      <div className="p-2">
-        <Card title="City Lights in New York" size="small">
-          Test
-        </Card>
-      </div>
-      <div className="p-2">
-        <Card title="City Lights in New York" size="medium">
-          Test
-        </Card>
-      </div>
-      <div className="p-2">
-        <Card title="City Lights in New York" size="large">
-          Test
-        </Card>
-      </div>
-    </div>
+    <App />
   </React.StrictMode>
 );
