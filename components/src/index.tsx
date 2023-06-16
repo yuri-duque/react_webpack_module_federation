abaaf13f25151c0b576db07cb10b5111c5751843
yuri-duque <yurithielmann83@gmail.com>
2023-06-15 22:11:43 -0300

    using app component

--- components/src/index.tsx
+++ components/src/index.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import CartItem from "./CartItem";
-
-import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <CartItem quantity={1} add={() => {}} remove={() => {}} />
+    <App />
   </React.StrictMode>
 );
