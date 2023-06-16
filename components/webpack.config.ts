6fd58eb376b0d1d4f744ce4fda8cdb09b6073762
yuri-duque <yurithielmann83@gmail.com>
2023-06-16 20:57:21 -0300

    updating webpack file on component project

--- components/webpack.config.ts
+++ components/webpack.config.ts
@@ -1,6 +1,7 @@
 import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import path from "path";
 import pkg from "./package.json";
 
@@ -8,7 +9,7 @@ const { ModuleFederationPlugin } = webpack.container;
 const deps = pkg.dependencies;
 
 module.exports = {
-  entry: path.join(__dirname, "src", "index.tsx"),
+  entry: path.join(__dirname, "src", "index.ts"),
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
@@ -31,43 +32,35 @@ module.exports = {
         exclude: /node_modules/,
         use: ["babel-loader"],
       },
-      // {
-      //   test: /\.(css|s[ac]ss)$/i,
-      //   include: path.resolve(__dirname, "src"),
-      //   exclude: /node_modules/,
-      //   use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
-      // },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        include: path.resolve(__dirname, "src"),
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
     ],
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "components",
+      name: "components_mfe", // Não pode ser igual ao expose
       filename: "remoteEntry.js",
       exposes: {
-        "components/Cart": "./src/Cart/Cart",
+        "./components": "./src/Cart/Cart",
+      },
+      remotes: {
+        uiElements: "uiElements@http://localhost:3001/remoteEntry.js",
       },
       shared: {
         ...deps,
         react: { singleton: true, eager: true, requiredVersion: deps.react },
-        "react-dom": {
-          singleton: true,
-          eager: true,
-          requiredVersion: deps["react-dom"],
-        },
-        "react-router-dom": {
-          singleton: true,
-          eager: true,
-          requiredVersion: deps["react-router-dom"],
-        },
       },
     }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
-    // new MiniCssExtractPlugin({
-    //   filename: "[name].bundle.css",
-    //   chunkFilename: "[id].css",
-    // }),
+    new MiniCssExtractPlugin({
+      chunkFilename: `${pkg.name.split("/").pop()}-[name].css`,
+    }),
     new CleanWebpackPlugin(), // Um plugin webpack para remover / limpar sua (s) pasta (s) de construção.
     new webpack.HotModuleReplacementPlugin(), // Atualiza o navegador quando houver alterações no código
   ],
